a3195184449118ec68d1f061ab5152d06f5bee11
Frederico Martins Porto <martinsporto@gmail.com>
2018-04-08 19:45:37 -0300

    Copy data from previous set when a set is added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,26 @@ $(document).on('turbolinks:load', function () {
       Turbolinks.visit($(e.target).siblings('td.main').find('a').attr('href'));
     }
   });
+  $('a.add_fields')
+    .data('association-insertion-method', 'after')
+    .data('association-insertion-node', 'this');
+  $('.links')
+    .on('cocoon:after-insert', function (e, added) {
+      var target = added.parents('form').attr('id').includes('workout') ? '.workout' : '.routine';
+
+      var repsOld = $($(target + '_exercise_exercise_sets_reps')[1]);
+      var weightOld = $($(target + '_exercise_exercise_sets_weight')[1]);
+      var durationOld = $($(target + '_exercise_exercise_sets_duration')[1]);
+
+      var repsNew = added.find(target + '_exercise_exercise_sets_reps').first().find('input');
+      var weightNew = added.find(target + '_exercise_exercise_sets_weight').first().find('input');
+      var durationNew = added.find(target + '_exercise_exercise_sets_duration').first().find('input');
+
+      if (repsOld.length > 0) repsNew.val(repsOld.first().find('input').val());
+      if (weightOld.length > 0) weightNew.val(weightOld.first().find('input').val());
+      if (durationOld.length > 0) durationNew.val(durationOld.first().find('input').val());
+
+      repsNew.focus();
+      repsNew.select();
+    })
 });
