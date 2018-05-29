1d6d4c74e9405f7ad65f184372aec64e13d453e5
Frederico Martins Porto <martinsporto@gmail.com>
2018-05-29 12:31:22 -0300

    Improve clean data sql

--- db/sql/clean_data_by_user_id.sql
+++ db/sql/clean_data_by_user_id.sql
@@ -1,8 +1,8 @@
-delete from routine_exercises where routine_id in (select id from routines where user_id = 1);
-delete from workout_exercises where workout_id in (select id from workouts where user_id = 1);
-delete from routines where user_id = 1;
-delete from workouts where user_id = 1;
-delete from body_parts_exercises where body_part_id in (select id from body_parts where user_id = 1);
-delete from body_parts where user_id = 1;
-delete from exercises where exercise_category_id in (select id from exercise_categories where user_id = 1);
-delete from exercise_categories where user_id = 1;
+delete from workout_exercises where workout_id = (select id from workouts where user_id = (select id from users where email = 'a@a.com.br'));
+delete from workouts where user_id = (select id from users where email = 'a@a.com.br');
+delete from routine_exercises where routine_id = (select id from routines where user_id = (select id from users where email = 'a@a.com.br'));
+delete from routines where user_id = (select id from users where email = 'a@a.com.br');
+delete from body_parts_exercises where body_part_id in (select id from body_parts where user_id = (select id from users where email = 'a@a.com.br'));
+delete from body_parts where user_id = (select id from users where email = 'a@a.com.br');
+delete from exercises where exercise_category_id in (select id from exercise_categories where user_id = (select id from users where email = 'a@a.com.br'));
+delete from exercise_categories where user_id = (select id from users where email = 'a@a.com.br');
